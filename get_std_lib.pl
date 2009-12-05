96d71da058966f14db77ab2ad4629e8d1ef9559a
Johan Simonsson <github@fun-tech.se>
2009-12-05 15:07:29 +0100

    get usb lib

--- get_std_lib.pl
+++ get_std_lib.pl
@@ -7,6 +7,34 @@ use Cwd;
 use Archive::Zip;       
 use POSIX qw(strftime);
 
+
+if(!defined($ARGV[0])) {
+    print "Usage ./get_std_lib.pl [std/usb]\n";
+}
+
+
+#
+# First some basic config. 
+#
+
+if ( $ARGV[0] =~ /std/ ) {
+    $stdlibzip = "stm32f10x_stdperiph_lib.zip";
+    $url = "http://www.st.com/stonline/products/support/micro/files/stm32f10x_stdperiph_lib.zip";
+
+} elsif ( $ARGV[0] =~ /usb/ ) {
+    $stdlibzip = "stm32f10x_usb_lib.zip";
+    $url = "http://www.st.com/stonline/products/support/micro/files/um0424.zip";
+
+} else {
+    print "Usage ./get_std_lib.pl [std/usb]\n";
+    print "Error wrong arg\n";
+
+}
+
+$cmd_str = "wget ".$url." -O ".$stdlibzip; 
+
+
+
 ## A ISO-date string
 $date_now = strftime("%Y%m%d-%H%M%S", localtime);
 print $date_now ."\n";
@@ -22,13 +50,6 @@ $cwd_path = getcwd();
 $tmp_path = $cwd_path."/tmp/";
 mkpath $tmp_path; 
 chdir $tmp_path;
-
-#
-# First some basic config. 
-#
-$stdlibzip = "stm32f10x_stdperiph_lib.zip";
-$url = "http://www.st.com/stonline/products/support/micro/files/stm32f10x_stdperiph_lib.zip";
-$cmd_str = "wget ".$url." -O ".$stdlibzip; 
 
 
 #
@@ -59,8 +80,9 @@ my $zip = Archive::Zip->new($stdlibzip);
 foreach my $member ($zip->members)
 {
     if(!defined($first_dir_name) and $member->isDirectory) {
-        if( $member->fileName =~ /StdPeriph/) {
-            $first_dir_name = $member->fileName;
+        if( $member->fileName =~ /.*Libraries/) {
+            #$first_dir_name = $member->fileName;
+            $first_dir_name = $&;
         }
     }
 
@@ -103,16 +125,25 @@ foreach my $member ($zip->members)
 #
 
 #print "ZIP info: ".$first_dir_name."\n";
+#What lib is this?
+if( $first_dir_name  =~ /(STM32.*Lib)_V/) {
+    $lib_track = $1;
+} else {
+    die("Error, what lib is this? (".$first_dir_name.")\n");
+}
+print "Lib track: ".$lib_track."\n";
 
 if( $first_dir_name  =~ /V([0-9]{1,}).([0-9]{1,}).([0-9]{1,})/) {
-    $lib_ver = $&;
-    $lib_track = "StdPeriph_Lib_V".$1;
+    $lib_ver    = $&;
 } else {
     die("Error, what version is this? (".$first_dir_name.")\n");
 }
 
 print "Lib version: ".$lib_ver."\n";
 print "Lib track: ".$lib_track."\n";
+
+$git_tag = $lib_track."_".$lib_ver;
+print "Lib tag: ".$git_tag."\n";
 
 
 
@@ -122,7 +153,6 @@ print "Lib track: ".$lib_track."\n";
 chdir $cwd_path;
 
 
-
 # 
 # Is this tag already created?
 # If so just exit (we don't have anything more to do...
@@ -130,8 +160,8 @@ chdir $cwd_path;
 $cmd_str = "git tag";
 open(DATA, $cmd_str." 2>&1 |") || die "Failed: $!\n";
 while ( defined( my $line = <DATA> )  ) {
-    if($line =~ /$lib_ver/) {
-        die("This version (".$lib_ver.") is tagged... exit\n");
+    if($line =~ /$git_tag/) {
+        die("This version (".$git_tag.") is tagged... exit\n");
     }
 }
 close DATA;
@@ -177,26 +207,45 @@ closedir DIR;
 
 
 $lib_dir_name = "StdPeriph_Lib";
+mkpath $lib_dir_name;
 
-if(-e "tmp/".$first_dir_name)
+
+$lib_dir_from = "tmp/".$first_dir_name;
+
+if(not -e $lib_dir_from) {
+    die("Can't find from dir: ".$lib_dir_from."\n");
+}
+
+opendir DIR, $lib_dir_from;
+foreach $file (readdir(DIR))
 {
-    move("tmp/".$first_dir_name, $lib_dir_name);
+    $file_with_path = $lib_dir_from."/".$file;
+    if(-d $file_with_path)
+    {
+        if (
+            $file =~ /CMSIS/ or
+            $file =~ /STM32F10x_StdPeriph_Driver/ or
+            $file =~ /STM32_USB-FS-Device_Driver/ )
+        {
+            print "move: ".$file_with_path." to ".$lib_dir_name."/".$file."\n";
+            move($file_with_path, $lib_dir_name."/".$file);
+        }
+    }
+}
+closedir DIR;
+
+if(not -e $lib_dir_name) {
+    die("Can't find to dir: ".$lib_dir_name."\n");
 }
 
-if(-e $lib_dir_name) {
-    print "Dir is ok: ".$lib_dir_name."\n";
-} else {
-    die("Error: ".$lib_dir_name."\n");
-}
-
-
+die("OK");
 #
 # More error control...?
 #
 
 system("git add ".$lib_dir_name);
 system("git commit -a -m 'ST:s ".$lib_dir_name." ".$lib_ver."'");
-system("git tag '".$lib_ver."'");
+system("git tag -a '".$git_tag."' -m '".$lib_ver." ".$date_now."'");
 
 system("rm -rf tmp/");
 
